281cc1f8a2a961213d4d7c28bc500db33aa59e70
h4galaz <d.quaas@gmx.de>
2020-06-20 15:26:48 +0200

    changed some files

--- ractf/misc/spentalkux_done/spentalkux-0.9/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import find_packages, setup
-
-setup(
-    name="spentalkux",
-    version="0.9",
-    description="I dare you to import this",
-    packages=find_packages(),
-    install_requires=[""],
-)
--- ractf/misc/spentalkux_done/spentalkux-13.37/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import find_packages, setup
-
-setup(
-    name="spentalkux",
-    version="13.37",
-    description="I dare you to import this",
-    packages=find_packages(),
-    install_requires=[""],
-)
